e5396f62296ba709bb79d1474c8f6eca8dc3561d
Sophia <sgdelia31@gmail.com>
2026-09-04 17:06:32 -0400

    Entity class details

--- Entities.cs
+++ Entities.cs
@@ -2,9 +2,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FlightTracker.Entities
 {
-    public class Vehicle{}
-    public class Mission{}
-    public class TelemetryReading{}
+    public class Vehicle
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+    public class Mission
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+    public class TelemetryReading
+    {
+        public int Id { get; set; }
+        public int MissionId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+        public double BatteryPct { get; set; }
+        public double SignalStrength { get; set; }
+    }
     public class User{}
 
     public class AppContext: DbContext
